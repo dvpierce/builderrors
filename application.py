25d34409cf4b6d3d5999bc610064916159266e99
dvpierce <dvpierce@yahoo.com>
2020-08-02 18:04:00 -0500

    Create application.py

---
+++ application.py
@@ -0,0 +1,7 @@
+import time
+
+
+theString = "This is a very, very long string that should cause a style check error because it is more than 80 characters long..."
+print(theString)
+time.sleep(1)
+exit(0)
